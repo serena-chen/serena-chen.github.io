c1830e3c247bae344f0336b772dfafeaac7081b6
serena-chen <sache13@student.monash.edu>
2017-04-11 22:21:00 +1000

    add js onclick

---
+++ homepage/republish/assets/js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+
+
+
+  $('.aliceprofile').click(function() {
+     $('.alicecomments').toggleClass('showme');
+   });
+
+   $('.jessprofile').click(function() {
+      $('.jesscomments').toggleClass('showme');
+    });
+
+    $('.kimprofile').click(function() {
+       $('.kimcomments').toggleClass('showme');
+     });
+
+     $('.alice2profile').click(function() {
+        $('.alice2comments').toggleClass('showme');
+      });
+
+      $('.janeprofile').click(function() {
+         $('.janecomments').toggleClass('showme');
+       });
+
+       $('.anneprofile').click(function() {
+          $('.annecomments').toggleClass('showme');
+        });
+
+        $('.zoeprofile').click(function() {
+           $('.zoecomments').toggleClass('showme');
+         });
+
+         $('.danaprofile').click(function() {
+            $('.danacomments').toggleClass('showme');
+          });
+
+          $('.alexprofile').click(function() {
+             $('.alexcomments').toggleClass('showme');
+           });
+
+           $('.sophiaprofile').click(function() {
+              $('.sophiacomments').toggleClass('showme');
+            });
+
+            $('.becprofile').click(function() {
+               $('.beccomments').toggleClass('showme');
+             });
+
+             $('.bridgetprofile').click(function() {
+                $('.bridgetcomments').toggleClass('showme');
+              });
+
+              $('.stephprofile').click(function() {
+                 $('.stephcomments').toggleClass('showme');
+               });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
