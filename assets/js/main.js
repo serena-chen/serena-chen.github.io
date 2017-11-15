ba96ba75d9adcd71d479c7d8ba002086e68dfed0
serena-chen <sache13@student.monash.edu>
2017-11-16 01:24:48 +1100

    site live 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,41 +4,126 @@ $(document).ready(function() {
 // Add jQuery here
 
 
-$(function () {
-    $('.slideshow').each(function () {
-        // Instance of slideshow
-        var $slideshow = $(this);
-        // Set count of images in slideshow
-        $slideshow.find('.images-counter').text($slideshow.find('.slideshow-item').length);
-        // Hide all but first image
-        $slideshow.find('.slideshow-item').not(':first').hide(0);
-        // Store the current image as data on the slideshow
-        $slideshow.data('data-image', 0);
-    });
+      $(function () {
+          $('.slideshow').each(function () {
+              // Instance of slideshow
+              var $slideshow = $(this);
+              // Set count of images in slideshow
+              $slideshow.find('.images-counter').text($slideshow.find('.slideshow-item').length);
+              // Hide all but first image
+              $slideshow.find('.slideshow-item').not(':first').hide(0);
+              // Store the current image as data on the slideshow
+              $slideshow.data('data-image', 0);
+          });
 
-    $('.slideshow .slideshow-item').click(function (e) {
-        // Find the parent slideshow
-        var $slideshow = $(this).closest('.slideshow');
-        // Get the images of that slideshow
-        var $images = $slideshow.find('.slideshow-item');
-        // Get current image from data - could also get from visibility of image
-        var currentImage = $slideshow.data('data-image');
-        // Get count of images
-        var imagesCounter = $images.length;
-        // Hide the current image
-        $images.eq(currentImage).hide(0);
-        // Increase the counter and wrap at the end
-        currentImage = currentImage >= imagesCounter - 1 ? 0 : currentImage + 1;
-        // Save the current image number
-        $slideshow.data('data-image', currentImage);
-        // Show the new image
-        $images.eq(currentImage).show(0);
-        // Set the text to match the count
-        $slideshow.find('.current-image-counter').text(currentImage + 1);
-        // Set the text to match the description in the image
-        $slideshow.find('.slideshow_comment').text($images.eq(currentImage).attr("alt"));
-    });
-});
+          $('.slideshow .slideshow-item').click(function (e) {
+              // Find the parent slideshow
+              var $slideshow = $(this).closest('.slideshow');
+              // Get the images of that slideshow
+              var $images = $slideshow.find('.slideshow-item');
+              // Get current image from data - could also get from visibility of image
+              var currentImage = $slideshow.data('data-image');
+              // Get count of images
+              var imagesCounter = $images.length;
+              // Hide the current image
+              $images.eq(currentImage).hide(0);
+              // Increase the counter and wrap at the end
+              currentImage = currentImage >= imagesCounter - 1 ? 0 : currentImage + 1;
+              // Save the current image number
+              $slideshow.data('data-image', currentImage);
+              // Show the new image
+              $images.eq(currentImage).show(0);
+              // Set the text to match the count
+              $slideshow.find('.current-image-counter').text(currentImage + 1);
+              // Set the text to match the description in the image
+              $slideshow.find('.slideshow_comment').text($images.eq(currentImage).attr("alt"));
+          });
+      });
+
+
+      // ON PAGE LOAD (ALL IMAGES SHOWN)
+      // $('.com-wrapper').addClass('hideme');
+
+
+      // SHOW PROJECT IMAGES ON NAV CLICK
+
+
+
+
+
+      $('.nav-exchange').click(function(event){
+        $('.item').removeClass('showproject');
+        $('.item').addClass('hideproject');
+
+        $('.item.exchange-images').removeClass('hideproject');
+        $('.item.exchange-images').addClass('showproject');
+
+        // $('.navitem').removeClass('highlight');
+        //
+        // $(this).addClass('highlight');
+      });
+
+
+      $('.nav-order').click(function(event){
+        $('.item').removeClass('showproject');
+        $('.item').addClass('hideproject');
+
+        $('.item.order-images').removeClass('hideproject');
+        $('.item.order-images').addClass('showproject');
+
+      });
+
+
+
+
+
+      // SHOW INFO POPUP
+
+
+      $('.info').click(function() {
+          $('.infopopup').addClass('showme');
+          $('.info').addClass('hideme');
+          $('.project-container').addClass('hideme');
+          $('.close').addClass('showme');
+
+          $('.menuitem').addClass('changecolor');
+      });
+
+      $('.close').click(function() {
+          $('.infopopup').removeClass('showme');
+          $('.info').removeClass('hideme');
+          $('.project-container').removeClass('hideme');
+          $('.close').removeClass('showme');
+
+          $('.menuitem').removeClass('changecolor');
+      });
+
+
+      // SHOW ASTERISKS ON LINK HOVER
+
+      $('.info').hover(function() {
+          $('.asterisk1').toggleClass('add-asterisk');
+      });
+
+      $('.insta').hover(function() {
+          $('.asterisk2').toggleClass('add-asterisk');
+      });
+
+      $('.mail').hover(function() {
+          $('.asterisk3').toggleClass('add-asterisk');
+      });
+
+      $('.moodboard').hover(function() {
+          $('.asterisk4').toggleClass('add-asterisk');
+      });
+
+      $('.experiment').hover(function() {
+          $('.asterisk5').toggleClass('add-asterisk');
+      });
+
+      $('.close').hover(function() {
+          $('.asterisk6').toggleClass('add-asterisk');
+      });
 
 
 
