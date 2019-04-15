a3427533cd9b02cf64efb0e7af67a288c81bc1b1
serena-chen <sache13@student.monash.edu>
2019-04-14 22:34:14 -0400

    vp project page mob styles 1

--- project-archive/vp/assets/js/main.js
+++ project-archive/vp/assets/js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 
 
 
-
 $('.header-menu').mouseenter(function() {
     $('.header-info').addClass('showme');
     $('.header-home').addClass('showme');
@@ -57,6 +56,61 @@ $('.biomicro-image').hover(function() {
     $('.biomicro-title').toggleClass('showme');
 
 });
+
+
+////// PROJECT PAGE ///////
+
+
+$(function () {
+    $('.slideshow').each(function () {
+        // Instance of slideshow
+        var $slideshow = $(this);
+        // Set count of images in slideshow
+        $slideshow.find('.images-counter').text($slideshow.find('.slideshow-item').length);
+        // Hide all but first image
+        $slideshow.find('.slideshow-item').not(':first').hide(0);
+        // Store the current image as data on the slideshow
+        $slideshow.data('data-image', 0);
+    });
+
+    $('.slideshow-item img').click(function (e) {
+        // Find the parent slideshow
+        var $slideshow = $(this).closest('.slideshow');
+        // Get the images of that slideshow
+        var $images = $slideshow.find('.slideshow-item');
+        // Get current image from data - could also get from visibility of image
+        var currentImage = $slideshow.data('data-image');
+        // Get count of images
+        var imagesCounter = $images.length;
+        // Hide the current image
+        $images.eq(currentImage).hide(0);
+        // Increase the counter and wrap at the end
+        currentImage = currentImage >= imagesCounter - 1 ? 0 : currentImage + 1;
+        // Save the current image number
+        $slideshow.data('data-image', currentImage);
+        // Show the new image
+        $images.eq(currentImage).show(0);
+        // Set the text to match the count
+        $slideshow.find('.current-image-counter').text(currentImage + 1);
+        // Set the text to match the description in the image
+        $slideshow.find('.slideshow_comment').text($images.eq(currentImage).attr("alt"));
+    });
+});
+
+
+
+/////////FOOTER
+
+$('.footer-project-title').click(function() {
+    $('.footer-project-description').toggleClass('showme');
+
+});
+
+// $('.footer-project-title').click(function() {
+//     $('.footer-project-description').toggleClass('hideme');
+//
+// });
+
 
 
 
