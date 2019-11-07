b873d8e49f783c1a41dd7e13f8b65c7b554832a6
serena-chen <sache13@student.monash.edu>
2019-11-07 01:14:41 -0500

    PUSH sun + moon time test

--- project-archive/ek/assets/js/home.js
+++ project-archive/ek/assets/js/home.js
@@ -11,25 +11,38 @@ $(function() {
 
 
 $('.client-link').click(function() {
-    $('.hidden-text').toggleClass('showme');
+    $('.hidden-text-client').toggleClass('showme');
+    $('body').toggleClass('changebg');
+
+});
+
+$('.project-link').click(function() {
+    $('.hidden-text-project').toggleClass('showme');
     $('body').toggleClass('changebg');
 
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   });
+
+
+
+
+////// PROJECT SLIDESHOW ///////
+
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
+var x = 2;
+function Slideshow(){
+    document.getElementById("img").src="./assets/images/ek_2.jpg";
+    x++;
+    if (x > 50)
+        x = 1;
+}
+
+document.getElementById ("button-click").addEventListener("click", Slideshow, false);
