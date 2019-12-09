5880f65ebce0d90021e3cc36bc2a0ad341531035
serena-chen <sache13@student.monash.edu>
2019-12-09 03:23:56 -0500

    scroll PNG test

--- project-archive/calliesberlin/v3.0/assets/js/home.js
+++ project-archive/calliesberlin/v3.0/assets/js/home.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 var frameNumber = 0, // start video at frame 0
       // lower numbers = faster playback
-      playbackConst = 2000,
+      playbackConst = 1000,
       // get page height from video duration
       setHeight = document.getElementById("set-height"),
       // select video element
